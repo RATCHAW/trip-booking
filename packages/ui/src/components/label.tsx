01fb3559b25d58720e738079be8380a0e8faa73f
ratchaw <bendarsiayoub@gmail.com>
2026-03-17 16:13:01 +0000

    refactor: update shadcn components to use new-york style instead of base-lyra

--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -1,12 +1,16 @@
 import { cn } from "@trip-booking/ui/lib/utils";
+import { Label as LabelPrimitive } from "radix-ui";
 import type * as React from "react";
 
-function Label({ className, ...props }: React.ComponentProps<"label">) {
+function Label({
+	className,
+	...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
 	return (
-		<label
+		<LabelPrimitive.Root
 			data-slot="label"
 			className={cn(
-				"flex select-none items-center gap-2 text-xs leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
+				"flex select-none items-center gap-2 font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
 				className,
 			)}
 			{...props}
